9bb249f885d5dc14a4e5fa3b12c1e47213bf2d02
HuangYanYu <695914800@qq.com>
2020-05-12 14:41:55 +0800

    feat:添加无状态组件

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,9 +3,17 @@ import {
   Link
 } from "react-router-dom";
 
+const HelloWorld = (props) => {
+  const sayHi = (event) => alert('Hello World')
+  return (
+    <div onClick={sayHi}>Hello World</div>
+  )
+}
+
 function Home() {
   return (
     <div>
+      <HelloWorld></HelloWorld>
       <div>This is Home!</div>
       <Link to='/About'>About</Link>
     </div>
